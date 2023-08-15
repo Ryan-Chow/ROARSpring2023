7c1cd990c3c3562a53075259134f74bad64185e0
iriya-chou <iriya.chou@gmail.com>
2023-08-14 17:54:52 -0700

    new waypoint
    increased waypoint from 3k to 9.5k
    dont cut the corners too close doesnt work

--- runner_competition_evaluator.py
+++ runner_competition_evaluator.py
@@ -10,7 +10,7 @@ from typing import Tuple
 from prettytable import PrettyTable
 from ROAR.agent_module.michael_pid_agent import PIDAgent
 from ROAR.agent_module.pid_fast_agent import PIDFastAgent
-
+#from ROAR.agent_module.special_agents.waypoint_generating_agent import WaypointGeneratigAgent
 
 
 def compute_score(carla_runner: CarlaRunner) -> Tuple[float, int, int]:
